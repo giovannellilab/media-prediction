490acdcc54e3085dbe99c264a2a3a1dd3f8737cf
guillecg <guillemcg92@gmail.com>
2024-05-22 11:49:16 +0200

    Move Taxon2EC-KEGG.ipynb into kegg.py

--- modules/kegg.py
+++ modules/kegg.py
@@ -77,3 +77,68 @@ def get_media2ec(ec_list: list) -> pd.DataFrame:
     )
 
     return results_df
+
+
+def get_taxon2ec(id_list: list) -> pd.DataFrame:
+    # Initialize results CSV file
+    results_path = os.path.join(
+        DATA_DIR,
+        "kegg",
+        "taxon2ec-kegg.csv"
+    )
+    record_columns = [
+        "Gene",
+        "EC",
+        "KEGG ID"
+    ]
+    pd.DataFrame(columns=record_columns).to_csv(
+        results_path,
+        index=False,
+        mode="w"
+    )
+
+    retries = Retry(
+        total=5,
+        backoff_factor=0.25,
+        status_forcelist=[500, 502, 503, 504]
+    )
+    session = requests.Session()
+    session.mount("https://", HTTPAdapter(max_retries=retries))
+
+    for id_idx, kegg_id in enumerate(id_list):
+
+        # print(f"[+] Retrieving EC numbers for organism: {kegg_id}")
+
+        url = f"https://rest.kegg.jp/link/ec/{kegg_id}"
+
+        response = session.get(url)
+        response.raise_for_status()
+
+        response_df = pd.read_table(
+            StringIO(response.text),
+            names=["Gene", "EC"]
+        )
+
+        # Remove leading "ec"
+        response_df["EC"] = response_df["EC"].str.replace("ec:", "")
+
+        # Add KEGG ID column
+        response_df["KEGG ID"] = kegg_id
+
+        # Save dataframe to file
+        response_df.to_csv(
+            results_path,
+            index=False,
+            header=False,
+            mode="a"
+        )
+
+        # Delete dataframe to save space
+        del response_df
+
+        if id_idx % 10 == 0:
+            print(
+                f"[+] Processed organism {kegg_id} ({id_idx} / {len(id_list)})"
+            )
+
+        time.sleep(0.25)

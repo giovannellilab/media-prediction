4622e65935dd50732f4451f72374169324abdace
wjlowe03 <wjlowe03@gmail.com>
2024-07-15 12:39:51 +0200

    Add ec2ko function to retrieve orthologs from an input list of ec numbers

--- notebooks/test/modules/kegg.py
+++ notebooks/test/modules/kegg.py
@@ -121,3 +121,57 @@ def compound2ec(id_list: list):
 
     return compound2ec_df
 
+
+### KEGG Orthology ###
+
+def ec2ko(id_list: list):
+    # Initialize an empty list to store parsed results
+    ec2ko_df = []
+
+    # Create a requests session
+    session = requests.Session()
+
+    # Iterate over compound IDs
+    for ec in id_list:
+        KEGG_url = f"https://rest.kegg.jp/get/ec:{ec}"
+        try:
+            response = session.get(KEGG_url)
+            response.raise_for_status()  # Raise an HTTPError for bad responses
+
+            # Parse the response using Biopython's REST module
+            compound_data = REST.kegg_get(ec).read()
+
+            # Extract relevant information
+            orthologs = []
+
+            # Split the response into lines and parse
+            lines = compound_data.split("\n")
+            capture = False
+            for line in lines:
+                if line.startswith("ORTHOLOGY"):
+                    capture = True
+                elif capture and not line.startswith(" "):
+                    capture = False
+                if capture:
+                    orthologs.append(line.strip())
+
+            # Combine ortholog lines into a single string with ';' separator
+            orthologs = '; '.join(orthologs).replace("ORTHOLOGY", "").strip()
+
+            # Append extracted information to results list
+            ec2ko_df.append(
+                pd.Series({
+                    "ec": ec,
+                    "KO": orthologs,
+                })
+            )
+        except requests.exceptions.HTTPError as http_err:
+            print(f"HTTP error occurred for {ec}: {http_err}")
+        except Exception as err:
+            print(f"Other error occurred for {ec}: {err}")
+
+    # Convert the list of Series into a DataFrame
+    ec2ko_df = pd.DataFrame(ec2ko_df)
+
+    return ec2ko_df
+

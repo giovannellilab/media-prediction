d82e295e2864f7d89b8337d8477455b861f9870d
wjlowe03 <wjlowe03@gmail.com>
2024-07-15 12:44:02 +0200

    Remove get_orthologs function dependencies

--- notebooks/test/modules/kegg.py
+++ notebooks/test/modules/kegg.py
@@ -7,54 +7,6 @@ from Bio.KEGG import REST
 
 
 
-def ec_info(ec_list: list):
-    retries = Retry(
-        total=5,
-        backoff_factor=0.25,
-        status_forcelist=[500, 502, 503, 504]
-    )
-    session = requests.Session()
-    session.mount("https://", HTTPAdapter(max_retries=retries))
-
-    results_df = []
-    
-    for ec_number in ec_list:
-        try:
-            url = f"https://rest.kegg.jp/get/ec:{ec_number}"
-            response = session.get(url)
-            response.raise_for_status()
-
-            for record in parse(StringIO(response.text)):
-                # Get orthologs
-                orth_df = get_orthologs(response.text)
-                orth_list = ";".join(orth_df["ID"].values)
-
-                results_df.append(
-                    pd.DataFrame([{
-                        "Entry": record.entry,
-                        "Name": record.name,
-                        "Orthologs": orth_list,
-                        "Cofactor": record.cofactor,
-                        "Pathway": record.pathway,
-                        "Reaction": record.reaction,
-                        "Product": record.product,
-                        "Genes": record.genes,
-                        "Structures": record.structures,
-                        "Substrate": record.substrate,
-                        "DBLinks": record.dblinks
-                    }])
-                )
-        except requests.exceptions.HTTPError as e:
-            print(f"HTTP error for EC number {ec_number}: {e}")
-        except Exception as e:
-            print(f"Error processing EC number {ec_number}: {e}")
-            
-       
-
-    return results_df
-
-
-
 ### KEGG Compound Mapping ### 
 
 def compound2ec(id_list: list):

3f30832099ccf2039b57af73ef44e02e5bc43847
wjlowe03 <wjlowe03@gmail.com>
2025-01-06 18:43:51 +0100

    Format changes, clearing redundant files

--- notebooks/Thesis/scripts/mmseqs2-convert-slurm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH --job-name="mmsconv"
-#SBATCH --time=160:00:00
-#SBATCH --cpus-per-task=80
-#SBATCH --mem=200G
-#SBATCH --partition=parallel
-
-mmseqs convertalis --threads $SLURM_CPUS_PER_TASK queryDB /path/to/UniRef90 resultDB resultDB.m8

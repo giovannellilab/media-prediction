cdc66119ca61b00f8e99129c3f5471afb415bb27
guillecg <guillemcg92@gmail.com>
2025-01-18 19:42:51 +0100

    Add initial MediaDive pipeline

---
+++ src/media_prediction/data_pipeline/pipeline.py
@@ -0,0 +1,30 @@
+import os
+
+import requests
+
+import pandas as pd
+
+from src.media_prediction.data_pipeline import mediadive as md
+
+
+def get_mediadive(data_dir: str) -> pd.DataFrame:
+
+    # Retrieve all available media from MediaDive
+    md_media_df = md.get_media()
+
+    # Create list of media IDs
+    media_id_list = md_media_df["media_id"].astype(str).unique()
+
+    # Retrieve media-associated strains
+    md_strains_df = md.get_strains(media_id_list)
+
+    # Merge media and strains IDs
+    data_df = pd.merge(
+        left=md_media_df,
+        right=md_strains_df,
+        on="media_id",
+        how="outer",
+        indicator=True
+    )
+
+    return data_df
